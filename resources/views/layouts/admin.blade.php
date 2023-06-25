2821ec04103c807a4295ca918cdc14f5828a8c7a
BangHD294 <hoangbang2941994@gmail.com>
2023-06-25 21:43:24 +0700

    create login logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,19 +3,34 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     @yield('title')
-
+{{--    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">--}}
+{{--    <link rel="stylesheet" href="{{asset("adminlte/plugins/fontawesome-free/css/all.min.css")}}">--}}
+{{--    <link rel="stylesheet" href="{{asset("adminlte/dist/css/adminlte.min.css")}}">--}}
+{{--    <link rel="stylesheet" href="{{asset("adminlte/plugins/fontawesome-free/css/all.min.css")}}">--}}
+{{--    <link rel="stylesheet" href="{{asset("adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css")}}">--}}
+{{--    <link rel="stylesheet" href="{{asset("adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css")}}">--}}
+{{--    <link rel="stylesheet" href="{{asset("adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css")}}">--}}
+{{--    <link rel="stylesheet" href="{{asset("adminlte/dist/css/adminlte.min.css")}}">--}}
+{{--    --}}
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <link rel="stylesheet" href="{{asset("adminlte/plugins/fontawesome-free/css/all.min.css")}}">
-    <link rel="stylesheet" href="{{asset("adminlte/dist/css/adminlte.min.css")}}">
-    <link rel="stylesheet" href="{{asset("adminlte/plugins/fontawesome-free/css/all.min.css")}}">
-    <!-- DataTables -->
-    <link rel="stylesheet" href="{{asset("adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css")}}">
-    <link rel="stylesheet" href="{{asset("adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css")}}">
-    <link rel="stylesheet" href="{{asset("adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css")}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset("adminlte/dist/css/adminlte.min.css")}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/select2/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/bs-stepper/css/bs-stepper.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/plugins/dropzone/min/dropzone.min.css')}}">
+    <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
+    @yield('css')
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
@@ -49,22 +64,40 @@
 <script src="{{asset("adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js")}}"></script>
 <script src="{{asset("adminlte/dist/js/adminlte.min.js")}}"></script>
 {{--<script src="{{asset("adminlte/dist/js/demo.js")}}"></script>--}}
-<script>
-    $(function () {
-        $("#example1").DataTable({
-            "responsive": true, "lengthChange": false, "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-        $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-        });
-    });
-</script>
+{{--<script>--}}
+{{--    $(function () {--}}
+{{--        $("#example1").DataTable({--}}
+{{--            "responsive": true, "lengthChange": false, "autoWidth": false,--}}
+{{--            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]--}}
+{{--        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');--}}
+{{--        $('#example2').DataTable({--}}
+{{--            "paging": true,--}}
+{{--            "lengthChange": false,--}}
+{{--            "searching": false,--}}
+{{--            "ordering": true,--}}
+{{--            "info": true,--}}
+{{--            "autoWidth": false,--}}
+{{--            "responsive": true,--}}
+{{--        });--}}
+{{--    });--}}
+{{--</script>--}}
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/daterangepicker/daterangepicker.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/select2/css/select2.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/bs-stepper/css/bs-stepper.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/plugins/dropzone/min/dropzone.min.css')}}">
+<link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
+@yield('css')
+<script src="{{ asset('js/app.js') }}" defer></script>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth'])->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-
-//Route::get('/home', function () {
-//    return view('home');
-//})->middleware(['auth'])->name('home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 
 Route::get('/home', function () {
@@ -101,4 +100,4 @@ Route::prefix('admin')->group(function () {
 //    });
 
 });
-//require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
