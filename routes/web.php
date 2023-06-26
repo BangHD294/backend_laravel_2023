6d43055740b56645f7bf52e5b5326a993c59e68a
BangHD294 <hoangbang2941994@gmail.com>
2023-06-26 22:01:29 +0700

    update code logout

--- routes/web.php
+++ routes/web.php
@@ -4,73 +4,72 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\AdminProductController;
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth'])->name('dashboard');
 
 
 Route::get('/home', function () {
     return view('home');
-});
+})->middleware(['auth'])->name('dashboard');;
 
 
 Route::prefix('admin')->group(function () {
     Route::prefix('categories')->group(function () {
         Route::get('/', [CategoryController::class, 'index'])
-            ->name('categories.index');
-//            ->middleware(['auth']);
+            ->name('categories.index')
+            ->middleware(['auth']);
 //
         Route::get('/create', [CategoryController::class, 'create'])
-            ->name('categories.create');
+            ->name('categories.create')
+            ->middleware(['auth']);;
 //
         Route::post('/store', [CategoryController::class, 'store'])
-            ->name('categories.store');
-//            ->middleware(['auth']);
+            ->name('categories.store')
+            ->middleware(['auth']);
 //
         Route::get('/edit/{id}', [CategoryController::class, 'edit'])
-            ->name('categories.edit');
-//            ->middleware(['auth']);
+            ->name('categories.edit')
+            ->middleware(['auth']);
 //
         Route::post('/update/{id}', [CategoryController::class, 'update'])
-            ->name('categories.update');
-//            ->middleware(['auth']);
+            ->name('categories.update')
+            ->middleware(['auth']);
 //
         Route::get('/delete/{id}', [CategoryController::class, 'delete'])
-            ->name('categories.delete');
-//            ->middleware(['auth']);
+            ->name('categories.delete')
+            ->middleware(['auth']);
     });
     Route::prefix('menus')->group(function () {
         Route::get('/',[MenuController::class, 'index'])
-            ->name('menus.index');
-//            ->middleware(['auth']);
+            ->name('menus.index')
+            ->middleware(['auth']);
 //
         Route::get('/create', [MenuController::class, 'create'])
-            ->name('menus.create');
-//            ->middleware(['auth']);
+            ->name('menus.create')
+            ->middleware(['auth']);
 //
         Route::post('/store', [MenuController::class, 'store'])
-            ->name('menus.store');
-//            ->middleware(['auth']);
+            ->name('menus.store')
+            ->middleware(['auth']);
 //
         Route::get('/edit/{id}',[MenuController::class, 'edit'])
-            ->name('menus.edit');
-//            ->middleware(['auth']);
+            ->name('menus.edit')
+            ->middleware(['auth']);
 //
         Route::post('/update/{id}',[MenuController::class, 'update'])
-            ->name('menus.update');
-//            ->middleware(['auth']);
+            ->name('menus.update')
+            ->middleware(['auth']);
 //
         Route::get('/delete/{id}',[MenuController::class, 'delete'])
-            ->name('menus.delete');
-//            ->middleware(['auth']);
+            ->name('menus.delete')
+            ->middleware(['auth']);
     });
 //
 //    Route::prefix('product')->group(function () {
